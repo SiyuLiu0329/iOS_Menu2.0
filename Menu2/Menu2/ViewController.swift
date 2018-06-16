8915a9d3861ff7c6bf8bd2e7409ab26e693d6dba
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-06-16 18:07:50 +1000

    inital commit

---
+++ Menu2/Menu2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Menu2
+//
+//  Created by Siyu Liu on 16/6/18.
+//  Copyright © 2018 Siyu Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
