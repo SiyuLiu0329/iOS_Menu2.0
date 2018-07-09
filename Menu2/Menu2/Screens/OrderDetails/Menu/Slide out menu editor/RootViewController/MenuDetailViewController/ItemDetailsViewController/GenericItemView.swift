67219394a45d4d223f786340284066015b765f0a
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-07-09 23:10:08 +1000

    item view layout

--- Menu2/Menu2/Screens/OrderDetails/Menu/Slide out menu editor/RootViewController/MenuDetailViewController/ItemDetailsViewController/GenericItemView.swift
+++ Menu2/Menu2/Screens/OrderDetails/Menu/Slide out menu editor/RootViewController/MenuDetailViewController/ItemDetailsViewController/GenericItemView.swift
@@ -11,9 +11,18 @@ import UIKit
 class GenericItemView: UIView {
     static let requiredHeightAndWidth: CGFloat = 120
     let colourBarHeight: CGFloat = 10
+    let imageViewHeight: CGFloat = 80
     var viewModel: GenericItemViewModel? {
         didSet {
             guard let viewModel = viewModel else { return }
+            if let image = viewModel.image {
+                itemImageView.image = image
+                itemImageView.frame = CGRect(x: 0, y: colourBarHeight, width: frame.width, height: imageViewHeight)
+                nameLabel.frame = CGRect(x: 0, y: 80 + colourBarHeight, width: frame.width, height: frame.height - imageViewHeight - colourBarHeight)
+            } else {
+                nameLabel.frame = CGRect(x: 0, y: colourBarHeight, width: frame.width, height: frame.height - colourBarHeight)
+                itemImageView.frame = CGRect.zero
+            }
             nameLabel.text = viewModel.name
         }
     }
@@ -27,15 +36,26 @@ class GenericItemView: UIView {
     private func layoutViews() {
         colourBar.frame = CGRect(x: 0, y: 0, width: frame.width, height: colourBarHeight)
     }
+
+    private var nameLabelFrameSplit: CGRect?
+    
+    var itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
     
     var nameLabel: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        label.numberOfLines = 0
         return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(colourBar)
+        addSubview(itemImageView)
         addSubview(nameLabel)
         backgroundColor = .white
         layer.cornerRadius = 5
